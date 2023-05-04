b0c6103d6ee6637caad6466e3630ac7bb8ea1f36
helloyeojin <bonuibomb@gmail.com>
2023-05-04 17:50:58 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/dto/ItemSearch.java
@@ -0,0 +1,16 @@
+package com.kbstar.dto;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class ItemSearch {
+    private int search_price1;
+    private int search_price2;
+}
